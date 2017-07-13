cb803d0e74425e367a4c79e1d1f38c5f57d0e7b0
Aleksey <ra9mig@gmail.com>
2017-07-13 17:16:28 +0600

    Update DS3231_sqw_32khz.ino

--- examples/DS3231_sqw_32khz/DS3231_sqw_32khz.ino
+++ examples/DS3231_sqw_32khz/DS3231_sqw_32khz.ino
@@ -1,22 +1,12 @@
-/*
-  DS3231: Real-Time Clock. SQW & 32kHz Example
-  Read more: www.jarzebski.pl/arduino/komponenty/zegar-czasu-rzeczywistego-rtc-ds3231.html
-  GIT: https://github.com/jarzebski/Arduino-DS3231
-  Web: http://www.jarzebski.pl
-  (c) 2014 by Korneliusz Jarzebski
-*/
-
 #include <Wire.h>
 #include <DS3231.h>
 
 DS3231 clock;
-
 boolean state;
 
 void setup()
 {
   Serial.begin(9600);
-
   // Initialize DS3231
   Serial.println("Initialize DS3231");;
   clock.begin();
@@ -26,19 +16,14 @@ void setup()
 
   // Select output as rate to 1Hz
   clock.setOutput(DS3231_1HZ);
-  
   // Enable output
   clock.enableOutput(true);
 
   // Check config
-
   if (clock.isOutput())
-  {
     Serial.println("Oscilator is enabled");
-  } else
-  {
+  else
     Serial.println("Oscilator is disabled");
-  }
 
   switch (clock.getOutput())
   {
@@ -46,7 +31,8 @@ void setup()
     case DS3231_4096HZ:  Serial.println("SQW = 4096Hz"); break;
     case DS3231_8192HZ:  Serial.println("SQW = 8192Hz"); break;
     case DS3231_32768HZ: Serial.println("SQW = 32768Hz"); break;
-    default: Serial.println("SQW = Unknown"); break; }
+    default: Serial.println("SQW = Unknown"); break; 
+  }
 }
 
 void loop()
@@ -54,5 +40,5 @@ void loop()
    // Togle 32kHz output
    clock.enable32kHz(state);
    state = !state;
-   delay(2000);
+   delay(2000); // 20sec
 }
