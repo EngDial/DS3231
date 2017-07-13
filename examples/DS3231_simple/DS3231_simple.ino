49271042ee2c9e665010519661206cf393f9b96e
Aleksey <ra9mig@gmail.com>
2017-07-13 17:20:55 +0600

    Update DS3231_simple.ino

--- examples/DS3231_simple/DS3231_simple.ino
+++ examples/DS3231_simple/DS3231_simple.ino
@@ -1,11 +1,3 @@
-/*
-  DS3231: Real-Time Clock. Simple example
-  Read more: www.jarzebski.pl/arduino/komponenty/zegar-czasu-rzeczywistego-rtc-ds3231.html
-  GIT: https://github.com/jarzebski/Arduino-DS3231
-  Web: http://www.jarzebski.pl
-  (c) 2014 by Korneliusz Jarzebski
-*/
-
 #include <Wire.h>
 #include <DS3231.h>
 
@@ -15,11 +7,9 @@ RTCDateTime dt;
 void setup()
 {
   Serial.begin(9600);
-
   // Initialize DS3231
   Serial.println("Initialize DS3231");;
   clock.begin();
-
   // Set sketch compiling time
   clock.setDateTime(__DATE__, __TIME__);
 }
@@ -27,9 +17,7 @@ void setup()
 void loop()
 {
   dt = clock.getDateTime();
-
   // For leading zero look to DS3231_dateformat example
-
   Serial.print("Raw data: ");
   Serial.print(dt.year);   Serial.print("-");
   Serial.print(dt.month);  Serial.print("-");
@@ -37,6 +25,5 @@ void loop()
   Serial.print(dt.hour);   Serial.print(":");
   Serial.print(dt.minute); Serial.print(":");
   Serial.print(dt.second); Serial.println("");
-
-  delay(1000);
+  delay(1000); // 1sec
 }
