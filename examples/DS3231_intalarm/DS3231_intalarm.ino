9c8a9f49f25fc469778453a72024b2ad4b161025
Aleksey <ra9mig@gmail.com>
2017-07-13 17:25:14 +0600

    Update DS3231_intalarm.ino

--- examples/DS3231_intalarm/DS3231_intalarm.ino
+++ examples/DS3231_intalarm/DS3231_intalarm.ino
@@ -1,11 +1,3 @@
-/*
-  DS3231: Real-Time Clock. Alarm simple
-  Read more: www.jarzebski.pl/arduino/komponenty/zegar-czasu-rzeczywistego-rtc-DS3231.html
-  GIT: https://github.com/jarzebski/Arduino-DS3231
-  Web: http://www.jarzebski.pl
-  (c) 2014 by Korneliusz Jarzebski
-*/
-
 #include <Wire.h>
 #include <DS3231.h>
 
@@ -24,28 +16,22 @@ void alarmFunction()
 void setup()
 {
   Serial.begin(9600);
-  
   // Initialize DS3231
   Serial.println("Initialize DS3231");;
   clock.begin();
- 
   // Disarm alarms and clear alarms for this example, because alarms is battery backed.
   // Under normal conditions, the settings should be reset after power and restart microcontroller.
   clock.armAlarm1(false);
   clock.armAlarm2(false);
   clock.clearAlarm1();
   clock.clearAlarm2();
-
   // Manual (Year, Month, Day, Hour, Minute, Second)
-  clock.setDateTime(2014, 4, 25, 0, 0, 0);
-
+  clock.setDateTime(2017, 7, 17, 7, 17, 7);
   // Set Alarm1 - Every 20s in each minute
   // setAlarm1(Date or Day, Hour, Minute, Second, Mode, Armed = true)
   clock.setAlarm1(0, 0, 0, 10, DS3231_MATCH_S);
-
   // Attach Interrput 0. In Arduino UNO connect DS3231 INT to Arduino Pin 2
-  attachInterrupt(0, alarmFunction, FALLING);
-
+  //attachInterrupt(0, alarmFunction, FALLING);
   // Setup LED Pin
   pinMode(alarmLED, OUTPUT);
 }
